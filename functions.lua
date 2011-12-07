5973a2258803b68b234e061349b01f548e348d3a
Dima <dima@penguin.ro>
2011-12-07 22:44:12 +0200

    Added first domain: tartauldesalcie.com

---
+++ functions.lua
@@ -0,0 +1,18 @@
+function google_app(domain)
+  -- Set mail exchangers
+  mx(domain, "aspmx.l.google.com", 5)
+  mx(domain, "alt1.aspmx.l.google.com", 10)
+  mx(domain, "alt2.aspmx.l.google.com", 10)
+  mx(domain, "aspmx2.l.google.com", 20)
+  mx(domain, "aspmx3.l.google.com", 20)
+  
+  -- Set mail.domain.com
+  cname(concat("calendar", domain), "ghs.google.com")
+  cname(concat("docs", domain), "ghs.google.com")
+  cname(concat("mail", domain), "ghs.google.com")
+  cname(concat("sites", domain), "ghs.google.com")
+  
+  -- SPF records
+  txt(domain, "google-site-verification=NWtHwvxSQDOgc9jiLqn3hwrF42l81lINMOFOHY35OCY")
+  txt(domain, "v=spf1 a mx include:_spf.google.com ~all")
+end
---
+++ tartauldesalcie.com.lua
@@ -0,0 +1,10 @@
+-- A records
+a(_a, "89.38.162.33")
+
+-- CNAME records
+cname("www", _a)
+cname("ftp", _a)
+
+-- Templates
+google_app(_a)
+
