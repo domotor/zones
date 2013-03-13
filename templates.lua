5ee169c6f0c94cb29c41c99516b85c9f5d039ef6
Dumitru Cherpec <dima@penguin.ro>
2013-03-13 23:54:33 +0200

    Update templates.lua
    Removed Google MX missing records

--- templates.lua
+++ templates.lua
@@ -3,8 +3,6 @@ function google_app(domain)
   mx(domain, "aspmx.l.google.com", 5)
   mx(domain, "alt1.aspmx.l.google.com", 10)
   mx(domain, "alt2.aspmx.l.google.com", 10)
-  mx(domain, "aspmx2.l.google.com", 20)
-  mx(domain, "aspmx3.l.google.com", 20)
   
   -- Set mail.domain.com
   cname(concat("calendar", domain), "ghs.google.com")
