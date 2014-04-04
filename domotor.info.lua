30dc80ac435f969ccc8a695f716be255ce939b3f
Dumitru Cherpec <dima@penguin.ro>
2014-04-04 23:21:57 +0300

    Test

--- domotor.info.lua
+++ domotor.info.lua
@@ -4,11 +4,12 @@ a("foto", "5.12.112.36")
 a("client", "5.12.112.36")
 a("vm", "198.211.125.18")
 a("trac", "198.211.125.18")
+a("trac", "198.211.125.18")
 
 -- CNAME records
 cname("www", _a)
 cname("ftp", _a)
-cname("dev", _a)
+cname("dev", _a, 300)
 
 -- Templates
 google_app(_a)
