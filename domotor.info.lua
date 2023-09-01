8f7c65431a4610cdbdd627173384a85c92987d72
Dumitru Cherpec <1017965+domotor@users.noreply.github.com>
2023-09-01 15:06:28 +0300

    Update domotor.info.lua

--- domotor.info.lua
+++ domotor.info.lua
@@ -1,5 +1,6 @@
 -- A records
 a(_a, "68.183.3.70")
+a("mail", "68.183.3.70")
 a("observium", "68.183.3.70")
 
 -- AAAA records
@@ -9,5 +10,8 @@ aaaa(_a, "2a03:b0c0:2:d0::101f:4001")
 cname("www", _a)
 cname("ftp", _a)
 
+-- MX records
+mx(_a, "mail.domotor.info", 10)
+
 -- SPF records
-txt(_a, "v=spf1 a -all")
+txt(_a, "v=spf1 mx a -all")
